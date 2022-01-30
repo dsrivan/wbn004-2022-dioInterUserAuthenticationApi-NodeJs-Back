d938b912d8d10f3bb6dbfa0405c6a8f068001521
Ivan da Silva Rosa <iivan.si@msn.com>
2022-01-30 12:27:09 -0300

    add rota status + endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
+import statusRoute from "./routes/status.route";
 import usersRoute from "./routes/users.route";
 
 const app = express();
-const PORT = 3000;
 
+// Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Configurações de rotas
 app.use(usersRoute);
+app.use(statusRoute);
 
+// Inicialização do servidor
+const PORT = 3000;
 app.listen(PORT, () => console.log(`App rodando na porta ${PORT}`));
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -4,7 +4,7 @@ import { StatusCodes } from "http-status-codes";
 const usersRoute = Router();
 
 usersRoute.get("/users", (req: Request, res: Response, next: NextFunction) => {
-  const users = [{ userName: "Ivan" }];
+  const users = [{ userName: "Ivan 1" }, { userName: "Ivan 2" }];
   res.status(StatusCodes.OK).send({ users });
 });
 
@@ -13,6 +13,36 @@ usersRoute.get(
   (req: Request, res: Response<{ uuid: string }>, next: NextFunction) => {
     const uuid = req.params.uuid;
     res.status(StatusCodes.OK).send({ uuid });
+  }
+);
+
+usersRoute.post("/users", (req: Request, res: Response, next: NextFunction) => {
+  const newUser = req.body;
+
+  res.status(StatusCodes.CREATED).send(newUser);
+});
+
+usersRoute.put(
+  "/users/:uuid",
+  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    const { userName } = req.body;
+
+    const putUser = {
+      uuid: uuid,
+      userName: `${userName} + ${uuid}`,
+    };
+
+    res.status(StatusCodes.OK).send({ putUser });
+  }
+);
+
+usersRoute.delete(
+  "/users/:uuid",
+  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+
+    res.status(StatusCodes.OK).send(`Success: Id ${uuid} deleted`);
   }
 );
 
